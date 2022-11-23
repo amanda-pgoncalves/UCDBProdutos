da6114f86ab4a2393ef0b98cf2ed7c6cc0b0e13b
amanda-pgoncalves <amandapinheirog@outlook.com>
2022-11-23 10:41:02 -0400

    Criação da estrutura do projeto

---
+++ src/UCDBProdutos.Application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UCDBProdutos.Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
